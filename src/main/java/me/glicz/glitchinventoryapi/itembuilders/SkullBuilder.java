cd9193cbd55442e1132b5b859eb097aa427fdf8f
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-01-26 21:30:33 +0100

    Add missing methods to SkullBuilder

--- src/main/java/me/glicz/glitchinventoryapi/itembuilders/SkullBuilder.java
+++ src/main/java/me/glicz/glitchinventoryapi/itembuilders/SkullBuilder.java
@@ -1,6 +1,8 @@
 package me.glicz.glitchinventoryapi.itembuilders;
 
 import com.google.common.io.BaseEncoding;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import lombok.SneakyThrows;
@@ -12,7 +14,7 @@ import org.bukkit.inventory.meta.SkullMeta;
 import java.lang.reflect.Field;
 import java.util.UUID;
 
-@SuppressWarnings({"deprecation", "unused"})
+@SuppressWarnings({"unused"})
 public class SkullBuilder extends ItemBuilder<SkullBuilder, SkullMeta> {
 
     private static final BaseEncoding base64 = BaseEncoding.base64();
@@ -35,8 +37,12 @@ public class SkullBuilder extends ItemBuilder<SkullBuilder, SkullMeta> {
     }
 
     public SkullBuilder setOwner(OfflinePlayer player) {
-        itemMeta.setOwnerProfile(player.getPlayerProfile());
+        itemMeta.setOwningPlayer(player);
         return this;
+    }
+
+    public OfflinePlayer getOwner() {
+        return itemMeta.getOwningPlayer();
     }
 
     @SneakyThrows
@@ -48,7 +54,28 @@ public class SkullBuilder extends ItemBuilder<SkullBuilder, SkullMeta> {
         return this;
     }
 
-    public SkullBuilder getUrl(String url) {
+    @SneakyThrows
+    public String getValue() {
+        GameProfile gameProfile = null;
+        try {
+            gameProfile = (GameProfile) profileField.get(itemMeta);
+        } catch (Exception ignored) { }
+        if (gameProfile == null)
+            return null;
+        for (Property property : gameProfile.getProperties().get("textures"))
+            if (property.getName().equals("textures")) return property.getValue();
+        return null;
+    }
+
+    public SkullBuilder setUrl(String url) {
         return setValue(base64.encode(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes()));
+    }
+
+    public String getUrl() {
+        String value = getValue();
+        if (value == null) return null;
+        String texturesJson = new String(base64.decode(value));
+        JsonObject jsonObject = new Gson().fromJson(texturesJson, JsonObject.class);
+        return jsonObject.getAsJsonObject("textures").getAsJsonObject("SKIN").get("url").getAsString();
     }
 }
