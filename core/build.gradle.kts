a52c02901edb3754adf5bc7a2f251cbb65059c47
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-03-18 15:03:05 +0100

    Add ClickType#getAsBukkit

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
 }
 
 tasks {
