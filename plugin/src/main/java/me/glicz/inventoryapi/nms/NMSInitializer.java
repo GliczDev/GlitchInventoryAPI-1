87a89c748f26be9a51a2d90607ef9cecde7714de
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-03-25 12:50:02 +0100

    Fix github url logging

--- plugin/src/main/java/me/glicz/inventoryapi/nms/NMSInitializer.java
+++ plugin/src/main/java/me/glicz/inventoryapi/nms/NMSInitializer.java
@@ -2,8 +2,6 @@ package me.glicz.inventoryapi.nms;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,14 +22,12 @@ public class NMSInitializer {
             return (NMS) nmsServerClass.getConstructors()[0].newInstance();
         } catch (ClassNotFoundException ex) {
             plugin.getLogger().severe("GlitchInventoryAPI DOES NOT support your server version!");
-            plugin.getComponentLogger().error(Component.text("If you think this is a bug, please report it on ")
-                    .append(Component.text("github").clickEvent(ClickEvent.openUrl("https://github.com/GliczDev/GlitchInventoryAPI/issues"))));
+            plugin.getLogger().severe("If you think this is a bug, please report it on github (https://github.com/GliczDev/GlitchInventoryAPI/issues)");
             Bukkit.getPluginManager().disablePlugin(plugin);
         } catch (InvocationTargetException | InstantiationException | IllegalAccessException ex) {
             ex.getCause().printStackTrace();
             plugin.getLogger().severe("Something went wrong while trying to create NMS object!");
-            plugin.getComponentLogger().error(Component.text("If you think this is a bug, please report it on ")
-                    .append(Component.text("github").clickEvent(ClickEvent.openUrl("https://github.com/GliczDev/GlitchInventoryAPI/issues"))));
+            plugin.getLogger().severe("If you think this is a bug, please report it on github (https://github.com/GliczDev/GlitchInventoryAPI/issues)");
             Bukkit.getPluginManager().disablePlugin(plugin);
         }
         throw new NullPointerException("Cannot get NMS object");
