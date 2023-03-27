baa87a45610ffd5d63550f9de31e7b0a3012025f
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-03-27 22:15:06 +0200

    Add `ifOpened` method with consumer

--- plugin/src/main/java/me/glicz/inventoryapi/inventories/OpenResult.java
+++ plugin/src/main/java/me/glicz/inventoryapi/inventories/OpenResult.java
@@ -1,4 +1,12 @@
 package me.glicz.inventoryapi.inventories;
 
+import java.util.function.Consumer;
+
 public record OpenResult<T extends GlitchInventory<T>>(boolean opened, GlitchInventory<T> inventory) {
+
+    public GlitchInventory<T> ifOpened(Consumer<GlitchInventory<T>> ifOpenedAction) {
+        if (opened)
+            ifOpenedAction.accept(inventory);
+        return inventory;
+    }
 }
