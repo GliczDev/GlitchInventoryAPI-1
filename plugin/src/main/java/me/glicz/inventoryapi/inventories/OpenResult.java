102d55af606524925e51d75b0447e6f56ef10268
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-04-15 16:00:05 +0200

    Fix OpenResult#ifOpened

--- plugin/src/main/java/me/glicz/inventoryapi/inventories/OpenResult.java
+++ plugin/src/main/java/me/glicz/inventoryapi/inventories/OpenResult.java
@@ -2,9 +2,9 @@ package me.glicz.inventoryapi.inventories;
 
 import java.util.function.Consumer;
 
-public record OpenResult<T extends GlitchInventory<T>>(boolean opened, GlitchInventory<T> inventory) {
+public record OpenResult<T extends GlitchInventory<T>>(boolean opened, T inventory) {
 
-    public GlitchInventory<T> ifOpened(Consumer<GlitchInventory<T>> ifOpenedAction) {
+    public T ifOpened(Consumer<T> ifOpenedAction) {
         if (opened)
             ifOpenedAction.accept(inventory);
         return inventory;
