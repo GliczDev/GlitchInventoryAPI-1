4f57d7f2a47d45682d67cdd96e8bc2fb4a17f012
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-04-16 21:39:32 +0200

    Add InventoryPageChangeEvent#hasNext and InventoryPageChangeEvent#hasPrevious

--- plugin/src/main/java/me/glicz/inventoryapi/events/paginated/InventoryPageChangeEvent.java
+++ plugin/src/main/java/me/glicz/inventoryapi/events/paginated/InventoryPageChangeEvent.java
@@ -1,19 +1,24 @@
 package me.glicz.inventoryapi.events.paginated;
 
 import lombok.Getter;
+import lombok.experimental.Accessors;
 import me.glicz.inventoryapi.events.InventoryEvent;
 import me.glicz.inventoryapi.inventories.PaginatedInventory;
 import org.bukkit.entity.Player;
 
+@Getter
 public class InventoryPageChangeEvent extends InventoryEvent {
 
-    @Getter
     private final int newPage, oldPage;
+    @Accessors(fluent = true)
+    private final boolean hasNext, hasPrevious;
 
     public InventoryPageChangeEvent(Player player, PaginatedInventory inventory, int newPage, int oldPage) {
         super(player, inventory);
         this.newPage = newPage;
         this.oldPage = oldPage;
+        this.hasNext = inventory.hasNextPage(player);
+        this.hasPrevious = inventory.hasPreviousPage(player);
     }
 
     @Override
