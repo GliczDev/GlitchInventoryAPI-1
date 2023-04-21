0b0e456c51ce7a9b723d7ca8b10a6c78224552f2
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-04-21 19:02:08 +0200

    Make GlitchInventoryAPIConfig constructor be protected

--- plugin/src/main/java/me/glicz/inventoryapi/GlitchInventoryAPIConfig.java
+++ plugin/src/main/java/me/glicz/inventoryapi/GlitchInventoryAPIConfig.java
@@ -1,8 +1,11 @@
 package me.glicz.inventoryapi;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GlitchInventoryAPIConfig {
 
     private boolean verifyInventoryIdOnClose;
