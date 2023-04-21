5f170e4614affcd8b57a10cbe4b4c0fb8bc8bab4
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-04-21 20:40:00 +0200

    Make GlitchInventoryAPIConfig setters return GlitchInventoryAPIConfig

--- plugin/src/main/java/me/glicz/inventoryapi/GlitchInventoryAPIConfig.java
+++ plugin/src/main/java/me/glicz/inventoryapi/GlitchInventoryAPIConfig.java
@@ -3,9 +3,11 @@ package me.glicz.inventoryapi;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Accessors(chain = true)
 public class GlitchInventoryAPIConfig {
 
     private boolean verifyInventoryIdOnClose;
