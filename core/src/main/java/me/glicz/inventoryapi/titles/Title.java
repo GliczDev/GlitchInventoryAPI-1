ff15200ff4f6e17878617157d4002a046a499628
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-04-26 18:56:32 +0200

    Title#simple with string arg

--- core/src/main/java/me/glicz/inventoryapi/titles/Title.java
+++ core/src/main/java/me/glicz/inventoryapi/titles/Title.java
@@ -4,6 +4,9 @@ import net.kyori.adventure.text.Component;
 
 public abstract class Title {
 
+    public static SimpleTitle simple(String title) {
+        return simple(Component.text(title));
+    }
     public static SimpleTitle simple(Component title) {
         return new SimpleTitle(title);
     }
