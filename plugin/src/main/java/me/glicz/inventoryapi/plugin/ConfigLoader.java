10437739ccb5171cbb14b45c834cd8942bcca503
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-05-03 20:41:58 +0200

    Fix ConfigLoader

--- plugin/src/main/java/me/glicz/inventoryapi/plugin/ConfigLoader.java
+++ plugin/src/main/java/me/glicz/inventoryapi/plugin/ConfigLoader.java
@@ -2,6 +2,7 @@ package me.glicz.inventoryapi.plugin;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -27,12 +28,11 @@ public class ConfigLoader {
                 .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
                 .enable(YAMLGenerator.Feature.MINIMIZE_QUOTES);
         ObjectMapper objectMapper = new ObjectMapper(factory)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
                 .setPropertyNamingStrategy(PropertyNamingStrategies.KEBAB_CASE);
-        if (!file.exists()) {
-            objectMapper.writeValue(file, GlitchInventoryAPI.getConfig());
-            return;
-        }
-        GlitchInventoryAPI.setConfig(objectMapper.readValue(file, GlitchInventoryAPIConfig.class));
+        if (file.exists())
+            GlitchInventoryAPI.setConfig(objectMapper.readValue(file, GlitchInventoryAPIConfig.class));
+        objectMapper.writeValue(file, GlitchInventoryAPI.getConfig());
     }
 }
